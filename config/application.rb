e1a530467b69975701a668ec52f7c9b215ff2a13
Daniel Ponce <d_ponce718@yahoo.com>
2015-07-06 14:01:34 -0400

    chaned config application to allow push to heroku

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module ReelMovement
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,5 +24,7 @@ module ReelMovement
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    
   end
 end
