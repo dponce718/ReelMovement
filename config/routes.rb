cfd8756f9f914746d6230a4c26bbf7159156231c
Daniel Ponce <d_ponce718@yahoo.com>
2015-07-06 15:56:56 -0400

    fix ssl

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ get 'signup' => 'users#new'
 
 resources :users
 
-config.force_ssl = true
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
