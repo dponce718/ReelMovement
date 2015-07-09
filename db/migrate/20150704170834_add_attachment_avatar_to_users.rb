b06dbfa50b27a1bfc7c4dc1062f129b9235baff5
Daniel Ponce <d_ponce718@yahoo.com>
2015-07-09 19:46:30 -0400

    fixed user table

--- db/migrate/20150704170834_add_attachment_avatar_to_users.rb
+++ db/migrate/20150704170834_add_attachment_avatar_to_users.rb
@@ -1,13 +1,9 @@
 class AddAttachmentAvatarToUsers < ActiveRecord::Migration
   def self.up
     change_table :users do |t|
-    	t.integer :user_id
       t.attachment :avatar
     end
   end
-
-  add_index :users, :user_id
-  add_attachment :users, :avatar
 
   def self.down
     remove_attachment :users, :avatar
