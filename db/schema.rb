6bd06d58910965a62e2b46a3f2d929fb1cf19c47
Daniel Ponce <d_ponce718@yahoo.com>
2015-08-22 18:58:14 -0400

    added purcahse model and database table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818191153) do
+ActiveRecord::Schema.define(version: 20150822225223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,18 @@ ActiveRecord::Schema.define(version: 20150818191153) do
   end
 
   add_index "programs", ["user_id"], name: "index_programs_on_user_id", using: :btree
+
+  create_table "purchases", force: :cascade do |t|
+    t.string   "email"
+    t.integer  "amount"
+    t.string   "description"
+    t.string   "currency"
+    t.string   "customer_id"
+    t.string   "card"
+    t.integer  "product_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "subscriptions", force: :cascade do |t|
     t.integer  "user_id"
