6322cea5182ee9234723322f7e8dd07403957c50
Daniel Ponce <d_ponce718@yahoo.com>
2015-08-28 16:42:29 -0400

    fix 2 test in edt_profile_spec.rb

--- spec/features/edit_profile_spec.rb
+++ spec/features/edit_profile_spec.rb
@@ -7,43 +7,44 @@ describe "edit profile" do
 	
 	it "logs out the user and send them to the root page" do
 		 @user = FactoryGirl.create(:user, email: "daniel@yahoo.com", password: "password")
-	     visit 'login'
-		 fill_in "email", with: "daniel@yahoo.com"
-       	 fill_in "password", with: "password"
+	     visit '/'
+	     click_on 'Log in'
+		 fill_in "Email", with: "daniel@yahoo.com"
+       	 fill_in "Password", with: "password"
 		 click_button "Log in"
 		 click_on 'Account'
-	     click_on 'Edit'
-	     expect(page).to have_content("Update your profile")
+	     click_on 'Edit registration'
+	     expect(page).to have_content("Edit User")
 	end	
 
 	it " unsuccessful edit password to short" do
 		 @user = FactoryGirl.create(:user, email: "daniel@yahoo.com", password: "password")
-	     visit 'login'
-		 fill_in "email", with: "daniel@yahoo.com"
-       	 fill_in "password", with: "password"
+	     visit '/'
+	     click_on 'Log in'
+		 fill_in "Email", with: "daniel@yahoo.com"
+       	 fill_in "Password", with: "password"
 		 click_button "Log in"
 		 click_on 'Account'
 	     click_on 'Edit'
-	     click_button 'Save changes'
+	     click_button 'Update'
 	     expect(page).to have_content("Password is too short")
 	 end    
 
 	 it "successful edit" do
 	 	@user = FactoryGirl.create(:user, email: "daniel@yahoo.com", password: "password")
-	 	old_password_digest = @user.password_digest
-	     visit 'login'
-		 fill_in "email", with: "daniel@yahoo.com"
-       	 fill_in "password", with: "password"
+	     visit '/'
+	     click_on 'Log in'
+		 fill_in "Email", with: "daniel@yahoo.com"
+       	 fill_in "Password", with: "password"
 		 click_button "Log in"
 		 click_on 'Account'
-	     click_on 'Edit'
-	     fill_in "Name", with: "daniel"
+	     click_on 'Edit registration'
 	     fill_in "Email", with: "daniel@yahoo.com"
-	     fill_in "user_password", with: "password1234"
-	     fill_in "user_password_confirmation", with: "password1234"
-	     click_button 'Save changes'	
-	     expect(page).to have_content("Profile updated")
-	     expect(@user.reload.password_digest).not_to eq old_password_digest
+	     fill_in "Password", with: "password1234"
+	     fill_in "Password confirmation", with: "password1234"
+	     fill_in "Current password", with: "password"
+	     click_button 'Update'	
+	     expect(page).to have_content("Your account has been updated successfully")
 	  end	
 
 	  it "requires user to be logged in " do
