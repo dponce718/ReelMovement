74c02d90622fbd4dfdc2fe8f28fab598c7d7179b
Daniel Ponce <d_ponce718@yahoo.com>
2015-08-30 17:40:31 -0400

    added a monthly description with a two week free trial

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,19 +5,20 @@ class ChargesController < ApplicationController
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
-	    :card  => params[:stripeToken]
+	    :card  => params[:stripeToken],
+	    :plan  => "unlimited"
 	  )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => params[:amount],
-	    :description => 'Growth Hacking Crash Course',
-	    :currency    => 'usd'
-	  )
+	 # charge = Stripe::Charge.create(
+	  #  :customer    => customer.id,
+	   # :amount      => params[:amount],
+	    #:description => 'Growth Hacking Crash Course',
+	    #:currency    => 'usd'
+	 # )
 
 	  purchase = Purchase.create(email: params[:stripeEmail],
 		card: params[:stripeToken], amount: params[:amount],
-		description: charge.description, currency: charge.currency,
+		description: "Reel Movement program", currency: "usd",
 		customer_id: customer.id, product_id: 1)
 
 	  redirect_to purchase
